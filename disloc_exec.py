b66773b2efd56711fe816d212f47ced191c91432
Jun Wang <wang208@indiana.edu>
2018-09-04 13:18:34 -0400

    #9 execute disloc.c

---
+++ disloc_exec.py
@@ -0,0 +1,54 @@
+"""
+    disloc_exec.py
+        -- execute disloc.c with an input file
+"""
+
+import os
+import subprocess
+
+def getbinary():
+    """ get the location of disloc """
+    
+    # assume the same location as the script
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    disloc_binary = script_path + os.path.sep + "disloc"
+    return disloc_binary
+
+def exec_disloc(input, output, workdir=False):
+    """ execute disloc with input and output """
+
+    disloc_binary = getbinary()
+
+    if workdir:
+        os.chdir(workdir)
+    proc = subprocess.Popen([disloc_binary,input,output], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    outs, errs = proc.communicate()
+    if proc.returncode == 0:
+        exec_status = {"status":"success","error":""}
+    else:
+        exec_status = {"status":"failed","error":errs}
+
+    return exec_status
+
+
+def main():
+
+    #test case 1
+    input_1 = "test/simple.input"
+    output_1 = "test/simple.output.csv"
+    status = exec_disloc(input_1,output_1)
+    print(status)
+    #test case 2
+    input_2 = "test/simple.input"
+    output_2 = ""
+    status = exec_disloc(input_2,output_2)
+    print(status)
+    #test case 3
+    input_3 = "KO1.input"
+    output_3 = "KO1.input.csv"
+    status = exec_disloc(input_3,output_3,workdir="test")
+    print(status)
+
+
+if __name__ == '__main__':
+    main()
